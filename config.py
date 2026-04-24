b966cdc08f91a2b0ce66e0d00d17fa28c9c28da8
PrincipieCyupe <p.cyubahiro@alustudent.com>
2026-04-25 01:44:58 +0200

    Added where email will be coming from and configured to use BREVO

--- config.py
+++ config.py
@@ -33,6 +33,7 @@ class Config:
     MAIL_PORT = int(os.getenv("MAIL_PORT", 587))
     MAIL_USERNAME = os.getenv("MAIL_USERNAME", "")
     MAIL_PASSWORD = os.getenv("MAIL_PASSWORD", "")
+    MAIL_FROM = os.getenv("MAIL_FROM", "")
 
     # Branding
     BRAND_NAME = "Mpact"
